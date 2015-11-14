9a369711cd44b2347e3654bccc0e19621a32222d
Ben Peterson <benpeterson210@yahoo.com>
2015-11-14 00:04:40 -0800

    Adding user forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 class UsersController < ApplicationController
   def new
+  	@user = User.new
   end
+
+  def show
+  	@user = User.find(params[:id])
+  	# debugger  <--- add when you want to invoke byebug in the server prompt
+  end	
+
+  def edit
+  	@user = User.find(params[:id])
+  end
+
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+	
+	else
+		render 'new'
+	end
+  end
+
+  private
+
+  	def user_params
+  		params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  	end
+
 end
+
