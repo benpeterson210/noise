74743c9f4932e287fd9ce41f2022d78ce4ca98b8
Ben Peterson <benpeterson210@yahoo.com>
2015-11-15 23:26:06 -0800

    finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,8 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
+  		flash[:success] = "Welcome to Noise  yelo"
+  		redirect_to @user
 	
 	else
 		render 'new'
