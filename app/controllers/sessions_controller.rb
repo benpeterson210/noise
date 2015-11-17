be1c471dd5cfa92c296a9738ae16ffca615949e8
Ben Peterson <benpeterson210@yahoo.com>
2015-11-16 18:09:42 -0800

    Adding session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,16 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	user = User.find_by(email: params[:session][:email].downcase)
-  	if user && user.authenicate(params[:session][:password])
-		#log in and redirect to user's page
-	redirect_to 'users#show'
+  	ben = User.find_by(email: params[:session][:email].downcase) #ben (change name to "user" is found in the sessions helper)
+  	if ben && ben.authenticate(params[:session][:password])
+		  log_inMethod ben
+      redirect_to ben
   	else
-	flash.now[:alert] = "Invalid email/password combination"
-  	render 'new'
+	   flash.now[:alert] = "Invalid email/password combination"
+  	 render 'new'
   	end
   end
 
   def destroy
   end
+  
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,15 @@
 module SessionsHelper
+
+	def log_inMethod(ben)
+		session[:user_id] = ben.id
+	end
+
+	def current_user
+		@current_user ||= User.find_by(id: session[:user_id])
+	end
+
+	def logged_in?
+		!current_user.nil?
+	end
+
 end
