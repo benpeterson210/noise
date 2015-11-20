4688c8249792d9a13167fbdbbffa8b5335ee58c5
Ben Peterson <benpeterson210@yahoo.com>
2015-11-19 17:42:46 -0800

    user integration tests again invalid logins

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+
   def new
   	@user = User.new
   end
@@ -10,6 +12,19 @@ class UsersController < ApplicationController
 
   def edit
   	@user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile Updated"
+      redirect_to @user
+    else
+      render 'edit'
+      
+    end
+
+
   end
 
   def create
@@ -29,6 +44,13 @@ class UsersController < ApplicationController
   	def user_params
   		params.require(:user).permit(:name, :email, :password, :password_confirmation)
   	end
+
+    def logged_in_user
+      unless logged_in?
+        flash[:alert] = "Please log in."
+        redirect_to login_path
+      end
+    end
 
 end
 
